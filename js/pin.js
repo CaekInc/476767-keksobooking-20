96f3bd1f21c672a242d3f0f1b14e9b373c84f136
Caek <caekinc@gmail.com>
2020-07-24 22:08:16 +0300

    all crush

--- js/pin.js
+++ js/pin.js
@@ -2,9 +2,9 @@
 (function () {
   var blockMap = document.querySelector('.map');
   var fragment = document.createDocumentFragment();
-    for (var i = 0; i < window.data.createAdverts.length; i++) {
-  fragment.appendChild(window.card.createPin(window.data.createAdverts[i]));
-}
+  for (var i = 0; i < window.data.createAdverts.length; i++) {
+    fragment.appendChild(window.card.createPin(window.data.createAdverts[i]));
+  }
   var addFragment = function () {
     blockMap.appendChild(fragment);
   };
