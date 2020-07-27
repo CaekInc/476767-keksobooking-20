be525c00632cf366f2cc9e5f05488db9a4fe0057
Caek <caekinc@gmail.com>
2020-07-27 20:12:28 +0300

    fix

--- js/map.js
+++ js/map.js
@@ -1,11 +1,12 @@
 'use strict';
 (function () {
   var blockMap = document.querySelector('.map');
-  var pins = document.createDocumentFragment();
-  for (var i = 0; i < window.data.adverts.length; i++) {
-    pins.appendChild(window.pin.create(window.data.adverts[i]));
-  }
+
   var addFragment = function () {
+    var pins = document.createDocumentFragment();
+    for (var i = 0; i < window.data.adverts.length; i++) {
+      pins.appendChild(window.pin.create(window.data.adverts[i]));
+    }
     blockMap.appendChild(pins);
   };
   window.map = {
